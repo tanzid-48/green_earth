d873d0169db89702a5ca2bf71c9ff4a1ef435bb0
tanzid-48 <mdtanzid.525@gmail.com>
2026-03-06 07:40:36 +0600

    add loading spenierusin daisy khala

--- script.js
+++ script.js
@@ -7,6 +7,54 @@ const loadCategories = () => {
             displayCategories(data.categories)
         });
 }
+
+ const loadingSpinner= document.getElementById('loading-spinner')
+const loadTrees = () => {
+
+    loadingSpinner.classList.remove("hidden");
+    loadingSpinner.classList.add("flex")
+    const url = "https://openapi.programming-hero.com/api/plants";
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+             loadingSpinner.classList.remove("flex");
+            loadingSpinner.classList.add("hidden");
+            
+            displayTrees(data.plants);
+        })
+       
+}
+
+const displayTrees = (trees) => {
+    const treesContainer = document.getElementById('trees-container')
+    treesContainer.innerHTML = "";
+
+    trees.forEach(tree => {
+
+        const card = document.createElement("div");
+        card.className = "card bg-base-100 shadow-sm";
+        card.innerHTML = `
+    <figure>
+        <img src="${tree.image}" alt="${tree.name}" class="w-full h-40 object-cover rounded-t-2xl" />
+    </figure>
+    <div class="card-body p-4">
+        <h2 class="card-title text-base">${tree.name}</h2>
+        <p class="text-sm text-gray-500 line-clamp-2">${tree.description}</p>
+        <div class="flex justify-between items-center mt-2">
+            <span class="text-xs border border-green-400 text-green-500 rounded-full px-2 py-1">${tree.category}</span>
+            <h2 class="font-bold text-[#4ade80]">$${tree.price}</h2>
+        </div>
+        <button class="btn btn-success w-full rounded-full mt-2">Add to Cart</button>
+    </div>
+`;
+
+        treesContainer.append(card)
+
+    });
+
+}
+
+
 const displayCategories = (categories) => {
 
     const categoriesContainer = document.getElementById('categories-container');
@@ -26,3 +74,4 @@ const displayCategories = (categories) => {
 }
 
 loadCategories();
+loadTrees();
